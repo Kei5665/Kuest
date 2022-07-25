8fb83846a5182dead2a06c62cf00ff15e341bfa9
Kei <kt10195665@yahoo.ne.jp>
2022-07-26 03:54:25 +0900

    Add: クエストキャンセルできるようにキャンセルボタンを追加

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -15,4 +15,11 @@ class StampsController < ApplicationController
       redirect_to stamps_path, success: "クエストを開始します！"  
     end
   end
+
+  def destroy
+    post = Post.find(params[:id])
+    stamp = current_user.stamps.find_by(post_id: post.id)
+    stamp.destroy!
+    redirect_to stamps_path
+  end
 end
