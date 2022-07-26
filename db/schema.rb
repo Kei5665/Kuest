cc03b97ba4b20c337e0a446779f6519d907789e2
Kei <kt10195665@yahoo.ne.jp>
2022-07-26 10:27:41 +0900

    Fix: stampテーブルを削除、questテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_25_080953) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_26_012528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,15 +65,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_25_080953) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "stamps", force: :cascade do |t|
+  create_table "quests", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "post_id"
-    t.boolean "stamped", default: false
+    t.boolean "quest_cleared", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_stamps_on_post_id"
-    t.index ["user_id", "post_id"], name: "index_stamps_on_user_id_and_post_id", unique: true
-    t.index ["user_id"], name: "index_stamps_on_user_id"
+    t.index ["post_id"], name: "index_quests_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_quests_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_quests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -89,6 +89,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_25_080953) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "posts", "users"
-  add_foreign_key "stamps", "posts"
-  add_foreign_key "stamps", "users"
+  add_foreign_key "quests", "posts"
+  add_foreign_key "quests", "users"
 end
