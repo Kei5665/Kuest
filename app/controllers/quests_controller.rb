dd57b58879581995a7bd1efe3984b935c39ca83a
Kei <kt10195665@yahoo.ne.jp>
2022-07-26 10:51:04 +0900

    Fix: stampコントローラーをquestコントローラーに置き換え

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -2,7 +2,7 @@ class QuestsController < ApplicationController
   before_action :require_login, only: %i[index]
 
   def index
-    @posts = current_user.posts.joins(:stamps).where(quests: {quest_cleared: false})
+    @posts = current_user.posts.joins(:quests).where(quests: {quest_cleared: false})
     gon.json = @posts.to_json
   end
 
@@ -11,14 +11,14 @@ class QuestsController < ApplicationController
     if current_user.ordered_quests.include?(post)
       redirect_to root_path, warning: "このクエストは受注済みです"  
     else
-      current_user.stamps_posts << post
+      current_user.ordered_quests << post
       redirect_to quests_path, success: "クエストを開始します！"  
     end
   end
 
   def destroy
     post = Post.find(params[:id])
-    quest = current_user.stamps.find_by(post_id: post.id)
+    quest = current_user.ordered_quests.find_by(post_id: post.id)
     quest.destroy!
     redirect_to quests_path, success: "クエストをキャンセルしました！"  
   end
