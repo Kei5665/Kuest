ad5821436fb01cc35a14a72771a9c1aea007ce35
Kei <kt10195665@yahoo.ne.jp>
2022-07-26 19:43:01 +0900

    Fix: 管理画面の手直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_26_012528) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_26_102831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,7 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_26_012528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.datetime "date"
+    t.string "date"
     t.string "place"
     t.string "target"
     t.string "url"
