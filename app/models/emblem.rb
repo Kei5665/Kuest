e6d04e852246cf3ad4f85cd144b0ba0b6fd4ebe9
Kei <kt10195665@yahoo.ne.jp>
2022-08-10 06:31:43 +0900

    Add: エンブレムの関連付けロジックを作成

--- app/models/emblem.rb
+++ app/models/emblem.rb
@@ -1,4 +1,7 @@
 class Emblem < ApplicationRecord
   has_one_attached :emblem_image
-  has_many :users, dependent: :destroy
+
+  belongs_to :user
+  has_many :user_emblems, dependent: :destroy
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,23 @@ class User < ApplicationRecord
   has_many :quests, dependent: :destroy
   has_many :ordered_quests, through: :quests, source: :post
   has_many :emblems, dependent: :destroy
+  has_many :user_emblems, dependent: :destroy
+  has_many :current_emblems,through: :user_emblems, source: :emblem
 
+  def select_emblem(clear_num)
+
+    begginer = Emblem.first
+    intermediate = Emblem.second
+    advanced = Emblem.last
+
+    if  clear_num == 1
+      self.user_emblems.build(emblem_id: begginer.id)
+    elsif clear_num == 3
+      self.user_emblems.build(emblem_id: intermediate.id)
+    elsif clear_num == 5
+      self.user_emblems.build(emblem_id: advanced.id)
+    elsif
+      return
+    end
+  end
 end
