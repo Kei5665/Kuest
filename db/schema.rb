7f4f5cddd5f6457191c294e474bfe8ce371706c1
Kei <kt10195665@yahoo.ne.jp>
2022-08-11 19:48:26 +0900

    Add: ローマ綴りのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_11_103849) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_104623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_103849) do
     t.string "area_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "spelling", null: false
   end
 
   create_table "authentications", force: :cascade do |t|
