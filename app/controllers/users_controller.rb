f5e90bd105f0bff7e458cf2c497568e1f4b1246d
Kei <kt10195665@yahoo.ne.jp>
2022-08-25 16:08:58 +0900

    Fix: ユーザー編集画面を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,30 @@
 class UsersController < ApplicationController
-  before_action :set_current_user, only: %i[edit update]
+  before_action :set_current_user, only: %i[edit update show]
 
   def edit;end
 
   def update
     if @user.update(update_params)
-      redirect_to quests_path, success: "変更が成功しました"
+      redirect_to quests_path, success: "変更登録が成功しました"
     else
-      flash.now[:danger] = "変更に失敗しました"
+      flash.now[:danger] = "変更登録に失敗しました"
       render :edit
     end
   end
 
   def show
-    @post = current_user.ordered_quests.last
+    @post = @user.ordered_quests.last
 
-    clear_num = current_user.clear_num
-    @level_statement = current_user.select_emblem(clear_num)
+    clear_num = @user.clear_num
+    @level_statement = @user.select_emblem(clear_num)
 
-    @emblem = current_user.current_emblems.last
+    @emblem = @user.current_emblems.last
   end
 
   private
 
   def update_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name,:email,:password)
   end
 
   def set_current_user
